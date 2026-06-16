1bc31b7bfad2a96a7bd16f4383816bd4d4e643f7
dppaharee <dppaharee@gmail.com>
2026-06-16 22:03:03 +0545

    The program as modified with some additon to function blocks and structure.

--- ATM.c
+++ ATM.c
@@ -4,20 +4,47 @@
 struct Balance
 {
 	int bal;
-	char dateNtime[];	
+	char dateNtime[][5];
 };
 struct Accounts
 {
 	int accNo;
 	char name[50];
-	int pin[4];
+	int pin;//4 unique numbers
 	long long int balance;
 	char accType[50];
 	struct Balance transaction[];
-};
+} cmp;
+int flag=0;
 void  Authenticaton()
 {
-
+	FILE *ptr=fopen("acc_details.dat","r");
+	int accnum,pinnum,retry=0;
+Auth:
+	printf("\nEnter your Account number:");
+	scanf("%d",&accnum);
+	printf("\nEnter your PIN number:");
+	scanf("%d",&pinnum);
+	while (ptr!=EOF)
+	{
+		fread(&cmp,sizeof(struct Accounts),1,ptr);
+		if (cmp.accNo==accnum && pinnum==cmp.pin)
+		{
+			flag=1;
+			break;
+		}
+	}
+	if (flag==0 && retry>5)
+	{
+		printf("\nIncorrect PIN or account number please try again.");
+		retry++;
+		goto Auth;
+	}
+	else if (flag==0 && retry>5)
+	{
+		printf("\nYour choice of input has failed to access the account multiple times. So we have proceeded to end the application.");
+	}
+	fclose(ptr);
 }
 void Withdraw()
 {
@@ -59,27 +86,42 @@ int main() {
 int main()
 {
 	FILE *ptr=fopen("C:\\codes\\nodeRepo.bin","wb");//need to change wb to rb+ later also create a folder in C drive named codes in your computer
-
-  
-  /*AVOID THIS!!!
-  time_t now = time(NULL);
+	char choice;
+	printf("\t\t\t\t\t\tWelcome To KIST Sem1 Banking/ATM System");
+	printf("----------------------------------------------------------------------------------------------------------------------------------------------");
+	printf("\na. Withdraw Cash");
+	printf("\nb. Deposit Cash");
+	printf("\nc. Balance Inquiry");
+	printf("\nd. Balance Inquiry");
+	printf("----------------------------------------------------------------------------------------------------------------------------------------------");
+	printf("\nProceed to select an option: ");
+	fflush(stdin);
+	gets(choice);
+	/*AVOID THIS!!!
+	time_t now = time(NULL);
 	char *string =ctime(&now);
 	printf("%s\n",string);*/
- switch() 
-   	case 'a'://withdraw
-			
+	switch(choice)
+	{
+
+		case 'a'://withdraw
+
 			goto	STARTUP;
+			break;
 		case 'b'://deposit
-			
+
 			goto	STARTUP;
+			break;
 		case 'c'://balance inquiry
-			
+
 			goto	STARTUP;
+			break;
 		case 'd'://new acc creation
-			
+
 			goto	STARTUP;
-		case 'e':
-			exit(0);
-	}  
+			break;
+		default:
+			printf("\nGiven input is out of range, please select option from range (a to d).");
+	}
 	return 0;
 }
